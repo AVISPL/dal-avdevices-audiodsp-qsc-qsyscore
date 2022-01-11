fb4790f6c2fb5b027449d73e72b90fdb5c2e1a7b
HieuLM-AVISPL <HM15015@avispl.com>
2022-01-11 08:56:29 +0700

    SYAL-701 fix: fix all bugs from QA

--- src/main/java/com/avispl/symphony/dal/device/core510i/QSYSCoreCommunicator.java
+++ src/main/java/com/avispl/symphony/dal/device/core510i/QSYSCoreCommunicator.java
@@ -586,7 +586,7 @@ public class QSYSCoreCommunicator extends RestCommunicator implements Monitorabl
 			namedGainComponents[i] = namedGainComponents[i].trim();
 
 			if (namedGainComponents[i].matches(QSYSCoreConstant.SPECIAL_CHARS_PATTERN)) {
-				errorMessages.append("Component ").append(namedGainComponents[i]).append(" contains 1 of these special characters: ~ ! @ # $ % ^ & \\ ' or contains <?");
+				errorMessages.append("Component ").append(namedGainComponents[i]).append(" contains 1 of these special characters: ~ ! @ # $ % ^ & \\ ' or contains <? ");
 			}
 		}
 
